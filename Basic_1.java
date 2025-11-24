8ba7ed2463ce20418053f7ca03528cf4a8497a8d
hbk <anakyn@naver.com>
2025-11-24 11:01:05 +0900

    first commit

---
+++ Basic_1.java
@@ -0,0 +1,9 @@
+public class Basic_1 {
+
+    //아래에 프린트로 출력함 프로그램에서 제일 처음 실행되는 부분
+    public static void main(String[] args){
+        //학습하기 위해서 봅니다
+        System.out.println("hello");
+    }
+    
+}
